b6f7e2a4b8aa1b900186f7ebc6103055115213d4
Alireza002L9 <alirezaesmaili6035@gmail.com>
2024-05-13 13:26:59 +0330

    Adding a Command-line flag: go run ./cmd/web -addr=":9999"

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
-
-
 func main() {
+
+	addr := flag.String("addr", ":4000", "Http Network Address")
+
+	// Importantly, we use the flag.Parse() function to parse the command-line flag.
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any errors are
+	// encountered during parsing the application will be terminated.
+	flag.Parse()
+	// we also have flag.Int() , flag.Bool() and flag.Float64()
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -16,7 +26,10 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Print("Starting server on http://localhost:4000/")
-	err := http.ListenAndServe(":4000", mux)
+
+	// Ports 0-1023 are restricted and (typically) can only be used by services 
+	// which have root privileges.
+	log.Printf("Starting server on http://localhost%s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
